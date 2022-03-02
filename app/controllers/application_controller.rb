8a61fb7d319384f9441d4b2a94cc9499e832d3b5
Glayson Olivieri <glayson.murollo75@gmail.com>
2022-03-02 12:52:17 -0300

    Add I18n

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,22 @@ class ApplicationController < ActionController::Base
   include Pundit::Authorization
   helper_method :current_user
 
+  around_action :switch_locale
+
+  def switch_locale(&action)
+    locale = params[:locale] || I18n.default_locale
+    I18n.with_locale(locale, &action)
+  end
+
   def current_user
     @current_user
   end
 
   def set_current_user(user)
     @current_user = user
+  end
+
+  def default_url_options
+      { locale: I18n.locale }
   end
 end
